169edcd00e77ab23cca6613de22b9fdd02122150
ranicewin <ranicetan68@gmail.com>
2021-03-08 17:33:50 +0800

    Implement base thermostat styling

---
+++ thermostat/src/App.js
@@ -0,0 +1,12 @@
+import './index.css';
+import Thermostat from './Thermostat';
+
+function App() {
+  return (
+    <div className="container">
+      <Thermostat/>
+    </div>
+  );
+}
+
+export default App;
---
+++ thermostat/src/utils.js
@@ -0,0 +1,27 @@
+/**
+ * Helper functions
+ */
+
+// Helper function to convert polar co-ordinates to cartesian co-ordinates
+function polarToCartesian(centerX, centerY, radius, degrees) {
+    var radians = (degrees - 90) * Math.PI / 180.0;
+  
+    return {
+      x: centerX + (radius * Math.cos(radians)),
+      y: centerY + (radius * Math.sin(radians))
+    };
+}
+
+// Helper function for creating d attribute for path for circle arcs 
+export function describeArc(x, y, radius, startAngle, endAngle){
+
+    var start = polarToCartesian(x, y, radius, endAngle);
+    var end = polarToCartesian(x, y, radius, startAngle);
+
+    var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+
+    var d = [ "M", start.x, start.y, "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y ].join(" ");
+
+	// debug console.log(d);
+    return d;       
+}
